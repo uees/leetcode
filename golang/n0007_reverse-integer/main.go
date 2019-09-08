a4df9cf4219b46e41154093a1a2c0bc7436e256a
Wan <wan@debian.lan>
2019-09-08 22:43:10 +0800

    fix int32

--- golang/n0007_reverse-integer/main.go
+++ golang/n0007_reverse-integer/main.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
-type myInt int64
+// 假设我们的环境只能存储得下 32 位的有符号整数
+type myInt int32
+
+const maxInt = myInt((1 << 31) - 1)
+const minInt = myInt(-(1 << 31))
 
 func (x *myInt) pop() myInt {
 	remainder := *x % 10 // 余数
@@ -24,23 +28,32 @@ func main() {
 
 // 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
 func reverse(x int) int {
-	var result, remainder myInt
+	var result, pop myInt
 	var _x = myInt(x)
-	for {
-		if _x != 0 {
-			remainder = _x.pop()
-			result.append(remainder)
-		} else {
-			break
+	a := maxInt / 10
+	b := minInt / 10
+	m := maxInt % 10 // 7
+	n := minInt % 10 // -8
+	for _x != 0 {
+		pop = _x.pop()
+		// 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。
+		// 请根据这个假设，如果反转后整数溢出那么就返回 0。
+		if result > a || (result == a && pop > m) {
+			return 0
 		}
+		if result < b || (result == b && pop < n) {
+			return 0
+		}
+		result.append(pop)
 	}
 
-	// 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。
-	// 请根据这个假设，如果反转后整数溢出那么就返回 0。
+	/**
 	resultInt := int(result)
 	if int(int32(result)) != resultInt {
 		return 0
 	}
-
 	return resultInt
+	*/
+
+	return int(result)
 }
